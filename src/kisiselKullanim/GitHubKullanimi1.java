a7a153c0ad8d4de4e88cebc7cab3aa48e84bea57
Enes AYHAN <enesayhan3232@gmail.com>
2022-06-15 13:48:46 +0300

    ilk degisiklik

--- src/kisiselKullanim/GitHubKullanimi1.java
+++ src/kisiselKullanim/GitHubKullanimi1.java
@@ -3,5 +3,9 @@ package kisiselKullanim;
 public class GitHubKullanimi1 {
     public static void main(String[] args) {
         System.out.println("Hello World");
+
+        System.out.println("that's it");
+
     }
+
 }
